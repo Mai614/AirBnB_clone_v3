f8f3be1a94ea12e8ed176f3dfc8d4f5623be6235
Mai614 <maymohamed614@gmail.com>
2023-10-30 05:17:06 +0000

    base

--- models/__init__.py
+++ models/__init__.py
@@ -5,12 +5,17 @@ initialize the models package
 
 from os import getenv
 
+# Set the storage type based on the environment variable
+storage_t = getenv('HBNB_TYPE_STORAGE')
+
 # DBSTORAGE
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+if storage_t == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 # FILESTORAGE
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
+# Reload the storage engine
 storage.reload()
